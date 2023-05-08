351b585550b54f2966dd9b0a8049a0c97c22eef0
Caio <caio.scatolino@outlook.com>
2023-05-08 16:23:54 -0300

    preparando projeto

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,3 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
-
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
