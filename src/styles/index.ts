63f989232b9d9dcad12c210dfb4fcbd9dc6f04b7
Caio <caio.scatolino@outlook.com>
2023-05-09 16:35:46 -0300

    instalando stitches e iniciando

---
+++ src/styles/index.ts
@@ -0,0 +1,20 @@
+import { createStitches } from '@stitches/react'
+
+export const {
+    config,
+    styled,
+    createTheme,
+    globalCss,
+    keyframes,
+    getCssText,
+    theme,
+    css,
+} = createStitches({
+
+    theme: {
+        colors: {
+            rocketseat: '#8257e6'
+        }
+    }
+
+})
