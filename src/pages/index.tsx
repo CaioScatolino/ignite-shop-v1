4b1018f20bf305b59ddba02ced167472fe9f156d
Caio <caio.scatolino@outlook.com>
2023-05-11 16:42:19 -0300

    Aula 5 Estrutura da Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,29 @@
-import { styled } from '../styles'
+import { HomeContainer, Product } from "@/styles/pages/home";
+import Image from "next/image";
 
-const Button = styled('button', {
-  backgroundColor: '$green500',
-  borderRadius: 4,
-  border: 0,
-  padding: '4px 8px',
-
-  span: {
-    fontWeight: 'bold',
-  },
-
-  '&:hover': {
-    filter: 'brightness(0.8)'
-  },
-})
+import camiseta1 from '../assets/camisetas/1.png'
+import camiseta2 from '../assets/camisetas/2.png'
+import camiseta3 from '../assets/camisetas/3.png'
 
 export default function Home() {
-  return <Button> <span>Teste</span>Hello World!</Button>;
+  return <HomeContainer>
+    <Product>
+      <Image src={camiseta1}  width={520} height={480} alt=""/>
+
+      <footer>
+        <strong>
+          Camiseta X <span>R$ 79,90</span>
+        </strong>
+      </footer>
+    </Product>
+    <Product>
+      <Image src={camiseta2}  width={520} height={480} alt=""/>
+
+      <footer>
+        <strong>
+          Camiseta X <span>R$ 79,90</span>
+        </strong>
+      </footer>
+    </Product>
+  </HomeContainer>;
 }
---
+++ src/styles/pages/home.ts
@@ -0,0 +1,11 @@
+import { styled } from "..";
+
+export const HomeContainer =  styled('main', { 
+    display: 'flex',
+    gap: '3rem',
+    backgroundColor: 'red',
+})
+
+export const Product = styled('a', {
+
+})
