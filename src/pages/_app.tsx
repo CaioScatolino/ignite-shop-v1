16f0d907aaffd1d607d78b5f2b975e32732d203f
Caio <caio.scatolino@outlook.com>
2023-05-11 12:00:44 -0300

    Modulo 2 Aula 03

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,23 @@
 
 import { globalStyles } from "../styles/global";
 import { AppProps } from "next/app";
+import logoImg from '../assets/logo.svg';
+import Image from "next/image";
+import { Container, Header } from "@/styles/pages/app";
 
 globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
 
-  
-  return <Component {...pageProps} />
+
+  return (
+    <Container>
+     <Header> 
+      <Image src={logoImg} alt="" />
+     </Header>
+      
+
+      <Component {...pageProps} />
+    </Container>)
+
 }
