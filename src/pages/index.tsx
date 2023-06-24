bd24f040581c159dce3dbd8343392b6fcac8e83e
Caio <caio.scatolino@outlook.com>
2023-06-24 09:54:13 -0300

    Corrigindo bug da imagem e continuando aulas

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,13 +3,9 @@ import Image from "next/image";
 
 import { useKeenSlider } from 'keen-slider/react';
 
-import camiseta1 from '../assets/camisetas/1.png'
-import camiseta2 from '../assets/camisetas/2.png'
-import camiseta3 from '../assets/camisetas/3.png'
-
 import 'keen-slider/keen-slider.min.css'
 import { stripe } from "@/lib/stripe";
-import { GetServerSideProps } from "next";
+import { GetStaticProps } from "next";
 import Stripe from "stripe";
 
 interface HomeProps {
@@ -17,7 +13,7 @@ interface HomeProps {
 
     id: string;
     name: string;
-    imageUrL: string;
+    imageUrl: string;
     price: number;
 
   }[]
@@ -38,8 +34,8 @@ export default function Home({ products }: HomeProps) {
       {
         products.map(product => {
           return (
-            <Product key = {product.id} className="keen-slider__slide">
-              <Image src= {product.imageUrL} width={520} height={480} alt="" />
+            <Product key={product.id} className="keen-slider__slide">
+              <Image src={product.imageUrl} width={520} height={480} alt="" />
 
               <footer>
                 <strong> {product.name} </strong>
@@ -53,7 +49,7 @@ export default function Home({ products }: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async ({ }) => {
 
   const response = await stripe.products.list({
     expand: ['data.default_price']
@@ -69,7 +65,10 @@ export const getServerSideProps: GetServerSideProps = async () => {
       id: product.id,
       name: product.name,
       imageUrl: product.images[0],
-      price: price.unit_amount / 100, // vem em centavos
+      price: new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL',
+      }).format(price.unit_amount! / 100), // vem em centavos
     }
   })
 
@@ -78,6 +77,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       products,
-    }
+    },
+    revalidate: 60 * 60 * 2, // 2 hours
   }
 }
