554f8e2cd6c6082b57ad21b3d231055dfecb6285
Caio <caio.scatolino@outlook.com>
2023-07-11 16:35:33 -0300

    Atualizando aulas

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,5 +1,22 @@
+import { ImageContainer, SuccessContainer } from "@/styles/pages/success";
+import Link from "next/link";
+
 export default function Success() {
     return (
-        <h1>Success</h1>
+        <SuccessContainer>
+            <h1>Compra Efetuada</h1>
+
+            <ImageContainer>
+
+            </ImageContainer>
+
+            <p>
+                Uhuull <strong>Caio Scatolino</strong>, sua <strong>Camiseta Mamouse</strong> já está a caminho da sua casa.
+            </p>
+
+            <Link href="/">
+                Voltar ao catálogo
+            </Link>
+        </SuccessContainer>
     )
 }
